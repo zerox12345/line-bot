0a83a0cdb8b5bb3e24e4ea6544883129ee541f3c
TC Chen <zerox12345@hotmail.com>
2022-04-24 16:09:37 +0800

    Updated conversation part

--- app.py
+++ app.py
@@ -37,9 +37,16 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = '哩勒工三小'
+    if msg == 'hi':
+        r = 'hi'
+    elif msg == '你吃飯了嗎':
+        r = '還沒'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
